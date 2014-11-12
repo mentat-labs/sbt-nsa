7e4413095c5eef6ef1075bcb6c5939de1f3c94d1
Marko Elezovic <marko@element.hr>
2014-11-12 04:07:17 +0100

    Added Xlogs: -Xlog-free-terms -Xlog-free-types -Xlog-implicit-conversions -Xlog-implicits -Xlog-reflective-calls

---
+++ core/src/main/scala/hr/element/sbt/nsa/x/XLogFreeTerms.scala
@@ -0,0 +1,3 @@
+package hr.element.sbt.nsa
+
+case object `-Xlog-free-terms` extends JustSetting
---
+++ core/src/main/scala/hr/element/sbt/nsa/x/XLogReflectiveCalls.scala
@@ -0,0 +1,3 @@
+package hr.element.sbt.nsa
+
+case object `-Xlog-reflective-calls` extends JustSetting
